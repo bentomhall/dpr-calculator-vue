908075f8c6c5319bf258bf30381bd4f37914436a
Benjamin Hall <bentomhall@gmail.com>
2023-03-29 20:30:30 -0700

    set deploy location

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import vuetify from 'vite-plugin-vuetify'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vuetify()],
+  base: "/red-calculator/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
